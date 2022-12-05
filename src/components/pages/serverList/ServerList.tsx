cea14b918203ed8a28aa67670070afed06e7d291
Kamil Wereszko <k.wereszko@neurosys.com>
2022-12-05 04:32:49 +0100

    add sortdata function

--- src/components/pages/serverList/ServerList.tsx
+++ src/components/pages/serverList/ServerList.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { DataTable } from 'components/molecules';
 import { fetchServerList } from 'api';
 import { useAppDispatch, useAppSelector } from 'store/types';
@@ -8,8 +8,31 @@ import colors from 'themes/colors';
 
 const ServerList = () => {
   const { servers, error, loading } = useAppSelector(state => state.server);
+  const [sortedBy, setSorted] = useState({ sortedBy: '', order: '' });
 
   const dispatch = useAppDispatch();
+
+  const sortData = (type: 'distance' | 'name') => {
+    const isSame = type === sortedBy.sortedBy;
+
+    const order = isSame ? (sortedBy.order === 'desc' ? 'asc' : 'desc') : 'asc';
+
+    if (order === 'asc') {
+      const sortedArray = [...servers].sort((a, b) =>
+        type === 'distance' ? a.distance - b.distance : a.name.localeCompare(b.name)
+      );
+
+      dispatch(setServers(sortedArray));
+    } else {
+      const reversSortedArray = [...servers].sort((a, b) =>
+        type === 'distance' ? b.distance - a.distance : b.name.localeCompare(a.name)
+      );
+
+      dispatch(setServers(reversSortedArray));
+    }
+
+    setSorted({ sortedBy: type, order });
+  };
 
   useEffect(() => {
     dispatch(setLoading(true));
@@ -22,18 +45,16 @@ const ServerList = () => {
       .finally(() => dispatch(setLoading(false)));
   }, []);
 
-  console.log({ error, loading });
-
   return (
     <DataTable>
       <DataTable.Header>
-        <DataTable.Title title="Servers" />
-        <DataTable.Title title="Distance" />
+        <DataTable.Title title="Servers" onClick={() => sortData('name')} />
+        <DataTable.Title title="Distance" onClick={() => sortData('distance')} />
       </DataTable.Header>
       {loading && <ActivityIndicator />}
       {error && <DataTable.Item color={colors.red} text={error} />}
       {servers.map(({ distance, name }) => (
-        <DataTable.Row key={name}>
+        <DataTable.Row key={`${name}-${distance}`}>
           <DataTable.Item text={name} />
           <DataTable.Item text={distance} />
         </DataTable.Row>
